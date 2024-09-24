f965dbdcfea90c9510215ac44741161b14d54a2a
yash hase <yash.hase24@sanjivani.edu.in>
2024-09-24 21:38:38 +0530

    Add files via upload

---
+++ binery oprators.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  int a=8, b=3;
+  
+  //Addition Opertor 
+  cout<<"a+b= "<<(a+b)<<endl;
+  
+  //Subtraction Opertor
+  cout<<"a-b= "<<(a-b)<<endl;
+  
+  //Multiplication Opertor
+  cout<<"a*b= "<<(a*b)<<endl;
+  
+  //Division Opertor
+  cout<<"a/b= "<<(a/b)<<endl;
+  
+  //Modulo Opertor
+  cout<<"a%b= "<<(a%b)<<endl;
+  
+  return 0;
+  
+}
